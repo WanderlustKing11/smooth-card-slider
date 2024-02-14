f46f4b27a9fa52a50fe3d81fdbebda6fc61916b2
WanderlustKing11 <WanderlustKing11@gmail.com>
2024-02-13 19:42:29 -0500

    Add icons; add stack slide with animated transition

--- imNotGoodEnough.js
+++ imNotGoodEnough.js
@@ -2,21 +2,46 @@ let activeIndex = 0;
 
 const groups = document.getElementsByClassName('card-group');
 
+document.addEventListener('DOMContentLoaded', () => {
+  const loveButton = document.getElementById('love-button');
+  const hateButton = document.getElementById('hate-button');
+
+  loveButton.addEventListener('click', handleLoveClick);
+  hateButton.addEventListener('click', handleHateClick);
+});
+
 const handleLoveClick = () => {
   // Bump active index
-
-  const nextIndex = activeIndex + 1 <= groups.length ? activeIndex + 1 : 0;
-
+  const nextIndex = activeIndex + 1 <= groups.length - 1 ? activeIndex + 1 : 0;
   const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
         nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
 
-  // Active group becomes after
 
+  // Active group becomes after
   currentGroup.dataset.status = 'after';
 
+  // Next group becomes active-from-before
+  nextGroup.dataset.status = 'becoming-active-from-before';
+
   // Next group becomes active
+  setTimeout(() => {
+    nextGroup.dataset.status = 'active';
+    activeIndex = nextIndex;
+  }, 100);  
+}
 
-  nextGroup.dataset.status = 'active';
-
-  activeIndex = nextIndex;
+const handleHateClick = () => {
+  const nextIndex = activeIndex - 1 >= 0 ? activeIndex - 1 : groups.length - 1;
+  
+  const currentGroup = document.querySelector(`[data-index="${activeIndex}"]`),
+        nextGroup = document.querySelector(`[data-index="${nextIndex}"]`);
+  
+  currentGroup.dataset.status = "before";
+  
+  nextGroup.dataset.status = "becoming-active-from-after";
+  
+  setTimeout(() => {
+    nextGroup.dataset.status = "active";
+    activeIndex = nextIndex;
+  }, 100);
 }
